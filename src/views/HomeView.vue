4b22d0d63b5b7ffc3a367190c09b094551545b95
Maxim Shelepov <mimaishel@gmail.com>
2023-05-07 18:54:32 -0400

    Initial commit.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="container relative">
+    <nav class="absolute top-0 flex justify-between">
+    </nav>
+  </div>
+</template>
