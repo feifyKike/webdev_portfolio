75d52f25ad98d93322b6934750ccdd00487ae16d
Maxim Shelepov <mimaishel@gmail.com>
2023-05-08 12:41:33 -0400

    Adding navbar.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+    <nav>
+      <div class="flex flex-wrap items-center justify-between p-4">
+        <img src="../assets/images/signature_logo.png" height="112.6" width="196" class="drop-shadow-md">
+
+        <button @click="dropDownActive = !dropDownActive" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 lg:hidden">
+          <div v-if="!dropDownActive">
+            <Bars2Icon class="h-10 w-10"/>
+          </div>
+          <div v-else>
+            <XMarkIcon class="h-10 w-10"/>
+          </div>
+        </button>
+
+        <div v-show="dropDownActive" class="block w-full lg:hidden">
+          <ul>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4" aria-current="page">About</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Experience</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Work</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Contact</a>
+            </li>
+            <li>
+              <a href="#" class="block text-center bg-gray-300 py-2 px-6 drop-shadow-md">Resume</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="hidden w-full lg:block lg:w-auto items-center" id="navbar-default">
+          <ul class="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0">
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4" aria-current="page">About</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Experience</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Work</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4">Contact</a>
+            </li>
+            <li>
+              <a href="#" class="block bg-gray-300 py-2 px-6 drop-shadow-md">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script setup>
+import { Bars2Icon, XMarkIcon } from '@heroicons/vue/24/solid'
+import { ref } from 'vue';
+
+const dropDownActive = ref(false)
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
-<script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <div class="container relative">
-    <nav class="absolute top-0 flex justify-between">
-    </nav>
-  </div>
+  <Navbar />
 </template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+
+</script>
