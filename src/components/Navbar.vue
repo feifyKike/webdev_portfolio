1b9778c7bfe5542bb04ee28522a8f689c5900344
Maxim Shelepov <mimaishel@gmail.com>
2023-05-08 21:34:33 -0400

    Added Landing page.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
-      <div class="flex flex-wrap items-center justify-between p-4">
-        <img src="../assets/images/signature_logo.png" height="112.6" width="196" class="drop-shadow-md">
+      <div class="flex flex-wrap items-center justify-between mx-5 p-4">
+        <img src="../assets/images/signature_logo.png" height="100.6" width="184" class="drop-shadow-md">
 
         <button @click="dropDownActive = !dropDownActive" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 lg:hidden">
           <div v-if="!dropDownActive">
@@ -33,7 +33,7 @@
         </div>
 
         <div class="hidden w-full lg:block lg:w-auto items-center" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0">
+          <ul class="font-medium flex flex-col p-4 lg:p-0 mt-4 lg:flex-row lg:space-x-8 lg:mt-0">
             <li>
               <a href="#" class="block py-2 pl-3 pr-4" aria-current="page">About</a>
             </li>
@@ -47,7 +47,7 @@
               <a href="#" class="block py-2 pl-3 pr-4">Contact</a>
             </li>
             <li>
-              <a href="#" class="block bg-gray-300 py-2 px-6 drop-shadow-md">Resume</a>
+              <a href="#" class="block bg-gray-300 py-2 px-6 shadow-md">Resume</a>
             </li>
           </ul>
         </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,43 @@
 <template>
-  <Navbar />
+  <div class="min-h-screen">
+    <Navbar />
+    <div class="min-h-full mx-10">
+      <div class="grid grid-cols-6 min-h-full">
+        <div class="col-span-1 flex-none relative">
+          <ul class="fixed bottom-0 mb-7 flex flex-col space-y-4">
+            <li>
+              <a href=""><img class="h-50 w-50" src="../components/icons/github.png"/></a>
+            </li>
+            <li>
+              <a href=""><img class="h-50 w-50" src="../components/icons/linkedin.png"/></a>
+            </li>
+            <li>
+              <a href=""><img class="h-50 w-50" src="../components/icons/medium.png"/></a>
+            </li>
+            <li>
+              <a href=""><img class="h-50 w-50" src="../components/icons/stackoverflow.png"/></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-span-4 flex-initial flex flex-col items-center space-y-72 relative">
+          <div class="flex items-center space-x-5">
+            <img class="w-72 h-72 rounded-full" src="../assets/images/portrait.jpg"/>
+            <div>
+              <h1 class="text-4xl font-bold">Hello, my name is Maxim Shelepov</h1>
+              <p>I'm a web developer and a computer science student at NC State University</p>
+            </div>
+          </div>
+          <!-- <div>
+            <span class="text-3xl font-bold px-5">About Me</span>
+            <span class="h-5 border-b-2 border-black"></span>
+          </div> -->
+        </div>
+        <div class="col-span-1 flex-none relative">
+          <p class="fixed bottom-0 right-10 origin-top-right rotate-90">📍 Based in Raleigh, NC</p>
+        </div>
+      </div>
+  </div>
+  </div>
 </template>
 
 <script setup>
