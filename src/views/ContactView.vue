076289b1fc5011d2b3a4e9e6cbf6fbb451016049
Maxim Shelepov <mimaishel@gmail.com>
2023-05-15 18:18:27 -0400

    Contact page styling & iframe adding scroll.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="flex flex-col my-auto">
         <div v-if="content.formEmbedLink">
-            <iframe :src="content.formEmbedLink" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" class="mb-10 min-h-screen w-full">Loading...</iframe>
+            <iframe :src="content.formEmbedLink" frameborder="0" marginheight="0" marginwidth="0" class="mb-10 min-h-screen w-full">Loading...</iframe>
         </div>
         <div v-else class="space-y-8 mx-auto">
             <h1 class="text-3xl font-extrabold text-center">Get in Touch</h1>
             <p v-for="msg in content.externalLink.note">{{ msg }}<br/></p>
-            <div class="flex items-center justify-center space-x-5">
+            <div class="block sm:flex sm:items-center sm:justify-center sm:space-x-5">
                 <template v-if="content.externalLink.link.email">
                     <a :href="'mailto:'+content.externalLink.link.email">
-                        <button class="bg-gray-300 py-2 px-6 drop-shadow-lg">Send an Email</button>
+                        <button class="py-2 px-6 bg-button-color shadow-sm shadow-button-color text-white transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300">Send an Email</button>
                     </a>
-                    <p class="text-sm">{{ content.externalLink.responseTimeMessage }}</p>
+                    <p class="text-sm mt-8 sm:mt-0">{{ content.externalLink.responseTimeMessage }}</p>
                 </template>
                 <template v-else>
                     <a :href="content.externalLink.link.externalLink">
-                        <button class="bg-gray-300 py-2 px-6 drop-shadow-lg">Reach Out</button>
+                        <button class="py-2 px-6 bg-button-color shadow-sm shadow-button-color text-white transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300">Reach Out</button>
                     </a>
                 </template>
             </div>
